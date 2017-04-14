e13395fbd13fa49ccfc19ec350f74dd85ba9dcc7
braysia <kudo@kudo-air.attlocal.net>
2017-04-13 23:18:46 -0700

    apply added

--- celltk/apply.py
+++ celltk/apply.py
@@ -1,5 +1,8 @@
 """
-python celltk/subdetect.py -l c3/img_00000000* -f ring_dilation -o c4 -p MARGIN=0
+python celltk/apply.py -i c0/img_00000000* -l c2/img_00000000*
+
+TODO:
+need to deal with parent id
 """
 
 from scipy.ndimage import imread
@@ -12,6 +15,13 @@ import subdetect_operation
 import ast
 from utils.postprocess_utils import regionprops # set default parent and next as None
 from LabeledArray.labeledarray.labeledarray import LabeledArray
+from os.path import exists
+
+
+PROP_SAVE = ['area', 'cell_id', 'convex_area', 'cv_intensity',
+             'eccentricity', 'major_axis_length', 'minor_axis_length', 'max_intensity',
+             'mean_intensity', 'median_intensity', 'min_intensity', 'orientation',
+             'perimeter', 'solidity', 'std_intensity', 'total_intensity', 'x', 'y']
 
 
 def make_dirs(path):
@@ -26,35 +36,53 @@ def add_parent_id(labels, img, cells):
     return cells
 
 
-cells = regionprops(labels, img)
-cells = add_parent_id(labels, img, cells)
+def initialize_arr(store):
+    nframe = len(store)
+    cell_ids = [[i.cell_id for i in cells] for cells in store]
+    cell_ids = [i for j in cell_ids for i in j]
+    ncells = len(np.unique(cell_ids))
+    return np.zeros((len(PROP_SAVE), ncells, nframe))
+
+
+def make_labeledarray(store, obj_name, ch_name):
+    arr = initialize_arr(store)
+    for frame, cells in enumerate(store):
+        for cell in cells:
+            for pn, prop in enumerate(PROP_SAVE):
+                arr[pn, cell.cell_id-1, frame] = getattr(cell, prop)
+
+    label_list = []
+    for prop in PROP_SAVE:
+        label_list.append([ch_name, obj_name, prop])
+    return LabeledArray(arr, label_list)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--images", help="images", nargs="*", default=[])
     parser.add_argument("-l", "--labels", help="labels", nargs="+")
-    parser.add_argument("-o", "--output", help="output directory",
-                        type=str, default='temp')
+    parser.add_argument("-o", "--output", help="file name", type=str, default='temp')
     # parser.add_argument("-f", "--functions", help="functions", nargs="+")
     # parser.add_argument("-p", "--param", nargs="*", help="parameters", type=lambda kv: kv.split("="))
     args = parser.parse_args()
     make_dirs(args.output)
-    param = dict(args.param)
-    for key, value in param.iteritems():
-        param[key] = ast.literal_eval(value)
+    # param = dict(args.param)
+    # for key, value in param.iteritems():
+    #     param[key] = ast.literal_eval(value)
 
     ch_name, obj_name = basename(dirname(args.images[0])), basename(dirname(args.labels[0])), 
 
+    store = []
     for path, pathl in zip(args.images, args.labels):
         img, labels = imread(path), imread(pathl).astype(np.int32)
-        # for function in args.functions:
-        #     func = getattr(subdetect_operation, function)
-        #     if img is not None:
-        #         labels = func(labels0, img, **param)
-        #     else:
-        #         labels = func(labels0, **param)
-        tiff.imsave(join(args.output, basename(pathl)), labels.astype(np.int32))
+        store.append(regionprops(labels, img))
+
+    larr = make_labeledarray(store, obj_name, ch_name)
+    args.output = args.output + '.npz' if not args.output.endswith('.npz') else args.output
+    if exists(args.output):
+        ex_larr = LabeledArray().load(args.output)
+        larr = larr.vstack(ex_larr)
+    larr.save(args.output)
 
 
 if __name__ == "__main__":
