f2945a6b98cc5bcab3890b7b84f69f275e6c85cf
braysia <kudo@DN525f08.SUNet>
2017-06-02 12:51:04 -0700

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="celltk",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     author='Takamasa Kudo',
     author_email='kudo@stanford.edu',
