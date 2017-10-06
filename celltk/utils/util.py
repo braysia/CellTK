0318e55db5f09a518dba65ca391e5428ed9956bd
braysia <kudo@DN0a22b3a1.SUNet>
2017-10-06 15:04:41 -0700

    imread for tiff file

--- celltk/utils/util.py
+++ celltk/utils/util.py
@@ -1,12 +1,20 @@
 import numpy as np
 from scipy.ndimage import imread as imread0
+import tifffile as tiff
+
+
+def imread_check_tiff(path):
+    img = imread0(path)
+    if img.dtype == 'object':
+        img = tiff.imread(path)
+    return img
 
 
 def imread(path):
     if isinstance(path, tuple) or isinstance(path, list):
         st = []
         for p in path:
-            st.append(imread0(p))
+            st.append(imread_check_tiff(p))
         return np.dstack(st)
     else:
-        return imread0(path)
+        return imread_check_tiff(path)
