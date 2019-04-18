52902d30764432d4116825bcd4e34f68751b8f8a
kudo <kudo@stanford.edu>
2019-04-17 21:18:25 -0700

    phage_image_only_under_bac and remove_bac_from_img are now filter_with_labels. segment_bacteria_cst_filter/segment_bacteria_no_near are integrated with segment_bacteria. ring_dilation_buffer is integrated with ring_dilation

--- celltk/subdetect_operation.py
+++ celltk/subdetect_operation.py
@@ -15,7 +15,7 @@ from segment_operation import constant_thres
 np.random.seed(0)
 
 
-def ring_dilation(labels, MARGIN=0, RINGWIDTH=4):
+def ring_dilation(labels, MARGIN=0, RINGWIDTH=4, BUFFER=0):
     """Create a ring around label.
     :param RINGWIDTH (int): Width of rings
     :param MARGIN (int): A region of rings is ignored if they are within MARGIN pixels away from label.
@@ -29,11 +29,10 @@ def ring_dilation(labels, MARGIN=0, RINGWIDTH=4):
                [10,  0,  0,  0, 10],
                [ 0, 10, 10, 10,  0]], dtype=uint16)
     """
-    return dilate_to_cytoring(labels, RINGWIDTH, MARGIN)
-
-
-def ring_dilation_buffer(labels, MARGIN=0, RINGWIDTH=4, BUFFER=2):
-    return dilate_to_cytoring_buffer(labels, RINGWIDTH, MARGIN, BUFFER)
+    if BUFFER == 0:
+        return dilate_to_cytoring(labels, RINGWIDTH, MARGIN)
+    else:
+        return dilate_to_cytoring_buffer(labels, RINGWIDTH, MARGIN, BUFFER)
 
 
 def ring_dilation_above_thres(labels, img,  MARGIN=2, RINGWIDTH=4,
@@ -185,8 +184,10 @@ def cytoplasm_levelset(labels, img, niter=20, dt=-0.5, thres=0.5):
     cytolabels = convert_labels(temp, labels, cytolabels)
     return cytolabels
 
-def segment_bacteria(nuc, img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE=1000, MINAREA=5, dist=25):
-    """ Segment bacteria using high pass filter and assign to closest nucleus
+
+def segment_bacteria(nuc, img, slen=3, SIGMA=0.5, THRES=20, CLOSE=20, MINAREA=5,
+                     dist=25, SEGMENT='highpass', ASSIGN=True):
+    """ Segment bacteria using high pass filter or constant thresholding and assign to closest nucleus
 
     Args:
         nuc (numpy.ndarray): nuclear mask labels
@@ -194,44 +195,29 @@ def segment_bacteria(nuc, img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE
         slen (int): Size of Gaussian kernel
         SIGMA (float): Standard deviation for Gaussian kernel
         THRES (int): Threshold pixel intensity fo real signal 
-        CLOSE (int): Radius for disk used to return morphological closing of the image (dilation followed by erosion to remove dark spots and connect bright cracks)
+        CLOSE (int): Radius for disk used to return morphological closing of the image
+                     (dilation followed by erosion to remove dark spots and connect bright cracks)
         THRESCHANGE (int): argument unnecessary? 
         MINAREA (int): minimum area in pixels for a bacterium 
-    dist (int): acceptable distance bac can be from mask 
+        dist (int): acceptable distance bac can be from mask 
+        SEGMENT: choose one of 'highpass' or 'constant'.
+        ASSIGN: If False, it does not assign to closest nucleus. 
     Returns:
         labels (numpy.ndarray[np.uint16]): bacterial mask labels  
 
     """
-    labels = label_high_pass(img, slen=slen, SIGMA=SIGMA, THRES=THRES, CLOSE=3)
+    if SEGMENT == 'highpass':
+        labels = label_high_pass(img, slen=slen, SIGMA=SIGMA, THRES=THRES, CLOSE=3)
+    elif SEGMENT == 'constant':
+        labels = constant_thres(img, THRES=THRES)
+    if not ASSIGN:
+        return labels.astype(np.uint16)
     if labels.any():
-        labels, comb, nuc_prop, nuc_loc = label_nearest(img, labels, nuc,dist)
+        labels, comb, nuc_prop, nuc_loc = label_nearest(img, labels, nuc, dist)
     from skimage.morphology import remove_small_objects
     labels = remove_small_objects(labels, MINAREA)
     return labels.astype(np.uint16)
 
-def segment_bacteria_cst_filter(nuc, img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE=1000, MINAREA=5, dist=25):
-    """ Segment bacteria using constant threshold and assign to closest nucleus
-
-    Args:
-        nuc (numpy.ndarray): nuclear mask labels
-        img (numpy.ndarray): image in bacterial channel
-        slen (int): Size of Gaussian kernel
-        SIGMA (float): Standard deviation for Gaussian kernel
-        THRES (int): Threshold pixel intensity fo real signal 
-        CLOSE (int): Radius for disk used to return morphological closing of the image (dilation followed by erosion to remove dark spots and connect bright cracks)
-        THRESCHANGE (int): argument unnecessary? 
-        MINAREA (int): minimum area in pixels for a bacterium 
-    distance (int): acceptable distance bacteria can be away from mask
-    Returns:
-        labels (numpy.ndarray[np.uint16]): bacterial mask labels  
-
-    """
-    labels = constant_thres(img, THRES=THRES)
-    if labels.any():
-        labels, comb, nuc_prop, nuc_loc = label_nearest(img, labels, nuc,dist)
-    from skimage.morphology import remove_small_objects
-    labels = remove_small_objects(labels, MINAREA)
-    return labels.astype(np.uint16)
 
 def segment_bacteria_return_cyto_no_bac(nuc, img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE=1000, MINAREA=5, dist=25):
     """ Segment bacteria and assign to closest nucleus and return the mask back without regions containing bacteria
@@ -258,45 +244,19 @@ def segment_bacteria_return_cyto_no_bac(nuc, img, slen=3, SIGMA=0.5,THRES=20, CL
     labels = comb - labels 
     return labels.astype(np.uint16)
 
-def segment_bacteria_no_near(img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE=1000, MINAREA=5, dist=25):
-    """ Segment all bacteria in image, don't assign to closest nucleus
 
-    Args:
-        nuc (numpy.ndarray): nuclear mask labels
-        img (numpy.ndarray): image in bacterial channel
-        slen (int): Size of Gaussian kernel
-        SIGMA (float): Standard deviation for Gaussian kernel
-        THRES (int): Threshold pixel intensity fo real signal 
-        CLOSE (int): Radius for disk used to return morphological closing of the image (dilation followed by erosion to remove dark spots and connect bright cracks)
-        THRESCHANGE (int): argument unnecessary? 
-        MINAREA (int): minimum area in pixels for a bacterium 
-
-    Returns:
-        labels (numpy.ndarray[np.uint16]): bacterial mask labels  
-
+def filter_with_labels(labels, img, BG=True):
     """
-    labels = label_high_pass(img, slen=slen, SIGMA=SIGMA, THRES=THRES, CLOSE=3)
-    return labels.astype(np.uint16)
-
-def phage_image_only_under_bac(bac,img):
-    """ Modify image so that only regions under bacteria mask have values and rest of pixels are set to 0 
-    bac (numpy.ndarray): bacterial mask labels
-    img (numpy.ndarray): img in any channel
+    Modify an image so that background regions in labels have value 0
+    and the rest of the pixels have their original value.
+    Setting BG to False will set foreground regions to be 0.
     """
-    indices = bac==0
-    img[indices]=0 # set regions where there's no bacteria to 0 
-    return img.astype(float)
+    if BG:
+        img[labels == 0] = 0
+    if not BG:
+        img[labels > 0] = 0
+    return img
 
-def remove_bac_from_img(img,label):
-    """ Modify an image so that regions containing bacteria have value 0 and the rest of the pixels have their original value 
-    img (numpy.ndarray): img in any channel 
-    label (numpy.ndarray): bacterial mask labels
-    """
-    new_image = np.zeros(img.shape,np.float32)
-    coords = label > 0
-    img[coords] = 0 
-    new_image = img 
-    return new_image
 
 
 def segment_bacteria_repair(nuc, img, slen=3, SIGMA=0.5,THRES=20, CLOSE=20, THRESCHANGE=1000, MINAREA=5, dist=25):
